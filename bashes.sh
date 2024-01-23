374f8934009724c6d4261bdccbd0b19cff0f78dc
Mohamad mirzaee <67410020+mohamadmrzy@users.noreply.github.com>
2024-01-23 16:38:36 +0330

    Add files via upload

---
+++ bashes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/alarm.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/changepass.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/data.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/datasend.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/disable.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/enable.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/exp.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/onlineuser.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/renew.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/sendalarm.sh
+wget https://raw.githubusercontent.com/mohamadmrzy/myscript/main/status.sh
+
+chmod +x alarm.sh changepass.sh data.sh datasend.sh disable.sh enable.sh exp.sh onlineuser.sh renew.sh sendalarm.sh status.sh
