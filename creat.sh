6e315cb4f88f5b9295bf62490e72d42a53ecd626
Mohamad mirzaee <67410020+mohamadmrzy@users.noreply.github.com>
2024-01-25 10:30:13 +0330

    Update creat.sh

--- creat.sh
+++ creat.sh
@@ -13,21 +13,37 @@ sudo ufw enable
 echo "UsePAM yes" | sudo tee -a /etc/ssh/sshd_config
 
 
-echo "#!/bin/bash
+#!/bin/bash
 
+usernames=("vpn-1" "vpn-2" "vpn-3" "vpn-4" "vpn-5" "vpn-6" "vpn-7" "vpn-8" "vpn-9" "vpn-10" "vpn-11" "vpn-12" "vpn-13" "vpn-14" "vpn-15" "vpn-16" "vpn-17" "vpn-18" "vpn-19" "vpn-20" "vpn-21" "vpn-22" "vpn-23" "vpn-24" "vpn-25" "vpn-26" "vpn-27" "vpn-28" "vpn-29" "vpn-30")
+passwords=("856181" "789701" "768568" "910621" "720672" "839053" "258642" "975313" "643675" "911913" "163733" "228760" "114669" "187968" "619957" "659691" "732130" "129510" "709156" "390615" "712246" "925282" "259423" "728210" "774634" "937303" "110393" "858110" "107479" "467167")
+output_file="users.txt"
 
-echo \"Username:Password\" > users.txt
+# Create users and set passwords with chpasswd
+for ((i=0; i<${#usernames[@]}; i++)); do
+    username=${usernames[$i]}
+    password=${passwords[$i]}
 
-
-for i in {1..30}; do
-    password=\$(shuf -i 101550-983621 -n 1)
-    username=\"vpn-\$i\"
-    sudo useradd -m \$username --shell=/bin/false
-    echo \"\$username:\$password\" | sudo chpasswd
-    echo \"\$username:\$password\" >> users.txt
+    # Create user if it doesn't exist
+    if id "$username" &>/dev/null; then
+        echo "User $username already exists. Skipping..."
+    else
+        useradd -m $username
+        echo "$username:$password" | chpasswd
+        echo "User $username created with password $password."
+    fi
 done
 
-echo \"Users created successfully! The usernames and passwords are saved in users.txt file.\"" > user.sh
+# Echo the list of usernames and passwords to a file
+echo "List of Usernames and Passwords:" > "$output_file"
+for ((i=0; i<${#usernames[@]}; i++)); do
+    username=${usernames[$i]}
+    password=${passwords[$i]}
+    echo "$username:$password" >> "$output_file"
+done
+
+echo "Usernames and passwords have been saved to $output_file."
+
 
 
 echo "#!/bin/bash
